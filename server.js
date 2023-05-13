ea6273468c130e05796313e0a7bffa7702f86d01
Claudio Parra <101138305+cdp50@users.noreply.github.com>
2023-05-13 16:35:01 -0600

    fixed

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ bodyParser.urlencoded({ extended: false });
 app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Z-key');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
 
