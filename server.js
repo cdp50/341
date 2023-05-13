d119c903ba46a36e23810444e4e8882e08fdf787
Claudio Parra <101138305+cdp50@users.noreply.github.com>
2023-05-13 16:23:25 -0600

    fixed to work with React 2

--- server.js
+++ server.js
@@ -9,11 +9,8 @@ bodyParser.urlencoded({ extended: false });
 app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Z-key');
-  res.setHeader('Content-Type', 'application/json');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   next();
-});
+})
 
 const port = process.env.PORT;
 const host = process.env.HOST;
